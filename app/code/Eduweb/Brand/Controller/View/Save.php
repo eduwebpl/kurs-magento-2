4fdd0947cd1d92c8e4a10c9a59a9f7368256dc05
Patryk Walus <patryk.walus@ageno.pl>
2021-10-02 17:53:34 +0200

    21-finish

--- app/code/Eduweb/Brand/Controller/View/Save.php
+++ app/code/Eduweb/Brand/Controller/View/Save.php
@@ -3,6 +3,7 @@ declare(strict_types=1);
 
 namespace Eduweb\Brand\Controller\View;
 
+use Eduweb\Brand\Model\Brand;
 use Magento\Framework\App\Action\HttpGetActionInterface;
 use Magento\Framework\Controller\Result\Json;
 use Magento\Framework\Controller\ResultFactory;
@@ -11,15 +12,25 @@ class Save implements HttpGetActionInterface
 {
     protected ResultFactory $resultFactory;
 
-    public function __construct(ResultFactory $resultFactory)
+    protected \Eduweb\Brand\Model\BrandFactory $brandFactory;
+
+    public function __construct(ResultFactory $resultFactory, \Eduweb\Brand\Model\BrandFactory $brandFactory)
     {
         $this->resultFactory = $resultFactory;
+        $this->brandFactory = $brandFactory;
     }
 
     public function execute()
     {
+        /** @var Brand $brand */
+        $brand = $this->brandFactory->create();
+        $brand->setData('name', 'Tesla');
+        $brand->setData('url_key', 'tesla');
+        $brand->save();
+
         /** @var Json $result */
         $result = $this->resultFactory->create(ResultFactory::TYPE_JSON);
+        $result->setData($brand->getData());
 
         return $result;
     }
--- app/code/Eduweb/Brand/ViewModel/View.php
+++ app/code/Eduweb/Brand/ViewModel/View.php
@@ -3,15 +3,31 @@ declare(strict_types=1);
 
 namespace Eduweb\Brand\ViewModel;
 
+use Eduweb\Brand\Model\Brand;
+use Magento\Framework\App\RequestInterface;
 use Magento\Framework\View\Element\Block\ArgumentInterface;
 
 class View implements ArgumentInterface
 {
+    protected RequestInterface $request;
+
+    protected \Eduweb\Brand\Model\BrandFactory $brandFactory;
+
+    public function __construct(RequestInterface $request, \Eduweb\Brand\Model\BrandFactory $brandFactory)
+    {
+        $this->request = $request;
+        $this->brandFactory = $brandFactory;
+    }
+
     public function getBrandInfo()
     {
+        $id = $this->request->getParam('id');
+        /** @var Brand $brand */
+        $brand = $this->brandFactory->create();
+        $brand->load($id);
+        
         return [
-            'code' => 'tesla',
-            'name' => 'Tesla',
+            'name' => $brand->getData('name'),
             'collaborations' => [
                 'Elon Musk',
                 'Panasonic',
