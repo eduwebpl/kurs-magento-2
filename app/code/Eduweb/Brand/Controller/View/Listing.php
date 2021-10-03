eb1bd687f7f302b62b9556cf922545b3c171a84a
Patryk Walus <patryk.walus@ageno.pl>
2021-10-03 11:48:44 +0200

    28-finish

--- app/code/Eduweb/Brand/Controller/View/Listing.php
+++ app/code/Eduweb/Brand/Controller/View/Listing.php
@@ -3,8 +3,11 @@ declare(strict_types=1);
 
 namespace Eduweb\Brand\Controller\View;
 
+use Eduweb\Brand\Api\BrandRepositoryInterface;
 use Eduweb\Brand\Api\Data\BrandInterface;
 use Eduweb\Brand\Model\Brand;
+use Magento\Framework\Api\FilterBuilder;
+use Magento\Framework\Api\SearchCriteriaBuilder;
 use Magento\Framework\App\Action\HttpGetActionInterface;
 use Magento\Framework\Controller\Result\Json;
 use Magento\Framework\Controller\ResultFactory;
@@ -12,26 +15,40 @@ use Magento\Framework\Controller\ResultFactory;
 class Listing implements HttpGetActionInterface
 {
     protected ResultFactory $resultFactory;
-    protected \Eduweb\Brand\Model\ResourceModel\Brand\CollectionFactory $collectionFactory;
+    protected BrandRepositoryInterface $brandRepository;
+    protected SearchCriteriaBuilder $searchCriteriaBuilder;
+    protected FilterBuilder $filterBuilder;
 
     public function __construct(
         ResultFactory $resultFactory,
-        \Eduweb\Brand\Model\ResourceModel\Brand\CollectionFactory $collectionFactory
+        BrandRepositoryInterface $brandRepository,
+        SearchCriteriaBuilder $searchCriteriaBuilder,
+        FilterBuilder $filterBuilder
     ) {
         $this->resultFactory = $resultFactory;
-        $this->collectionFactory = $collectionFactory;
+        $this->brandRepository = $brandRepository;
+        $this->searchCriteriaBuilder = $searchCriteriaBuilder;
+        $this->filterBuilder = $filterBuilder;
     }
 
     public function execute()
     {
-        /** @var  \Eduweb\Brand\Model\ResourceModel\Brand\Collection $collection */
-        $collection = $this->collectionFactory->create();
-        $collection->addFieldToSelect(['name', 'url_key']);
+        $filter = $this->filterBuilder->setField('name')
+            ->setValue('%te%')
+            ->setConditionType('like')
+            ->create();
+
+        $filters = [$filter];
+        $this->searchCriteriaBuilder->addFilters($filters);
+
+        $searchCriteria = $this->searchCriteriaBuilder->create();
+
+        $items = $this->brandRepository->getList($searchCriteria)->getItems();
+
 
         $data = [];
-        /** @var Brand $brand */
-        foreach ($collection as $brand) {
-            $data[] = $brand->getData(BrandInterface::NAME);
+        foreach ($items as $brand) {
+            $data[] = $brand->getName();
         }
 
         /** @var Json $result */
