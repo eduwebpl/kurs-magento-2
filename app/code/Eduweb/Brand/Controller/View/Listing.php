c8b1df86940ed3902aab0501fe8dd61edd191c5a
Patryk Walus <patryk.walus@ageno.pl>
2021-10-03 10:43:33 +0200

    26

---
+++ app/code/Eduweb/Brand/Controller/View/Listing.php
@@ -0,0 +1,28 @@
+<?php
+declare(strict_types=1);
+
+namespace Eduweb\Brand\Controller\View;
+
+use Magento\Framework\App\Action\HttpGetActionInterface;
+use Magento\Framework\Controller\Result\Json;
+use Magento\Framework\Controller\ResultFactory;
+
+class Listing implements HttpGetActionInterface
+{
+    protected ResultFactory $resultFactory;
+
+    public function __construct(
+        ResultFactory $resultFactory
+    ) {
+        $this->resultFactory = $resultFactory;
+    }
+
+    public function execute()
+    {
+
+        /** @var Json $result */
+        $result = $this->resultFactory->create(ResultFactory::TYPE_JSON);
+
+        return $result;
+    }
+}
